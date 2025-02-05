b3abceb3a29282ca02b2b29dba62da1ff836e0de
LuArtD <luart950@gmail.com>
2025-02-05 20:34:28 +0100

    modificacion en app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from routes import router
+import uvicorn
 
 app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
